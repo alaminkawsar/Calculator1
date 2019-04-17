de05f19833e00dd0b92e5ffc77a4941c63f74612
Rifat <rifathosan01@gmail.com>
2019-04-17 10:29:38 +0600

    Division method created

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,18 @@
 
 public class Calculator{
+
+	public static double  Division(double fristvalue, double secondvalue){
+        
+        try{
+            double ans = fristvalue/secondvalue;
+			return ans;
+        }
+		catch (ArithmeticException aExp) { 
+             System.out.println("Division by zero.");
+            return 0;
+        }
+	}
+
 	public static void main(String args[]){
 		double number1=10,number2=5;
 		System.out.println(Addition(number1,number2));
@@ -7,5 +20,6 @@ public class Calculator{
 		System.out.println(Multiplication(number1,number2));
 		System.out.println(Division(number1,number2));
 	}
+
 	
 }
