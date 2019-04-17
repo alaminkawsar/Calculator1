be5a45654864fcc653c47c817e72ed506f7889e4
Al Amin Kawsar <alaminkawsar8643@gmail.com>
2019-04-17 10:22:28 +0600

    All function are called

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,11 @@
+
 public class Calculator{
 	public static void main(String args[]){
+		double number1=10,number2=5;
+		System.out.println(Addition(number1,number2));
+		System.out.println(Subtraction(number1,number2));
+		System.out.println(Multiplication(number1,number2));
+		System.out.println(Division(number1,number2));
 	}
+	
 }
